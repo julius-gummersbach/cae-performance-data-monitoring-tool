0899262857af3e7df9de1a9e2774e5565577263d
Julius Gummersbach <gummersbach@se-rwth.de>
2022-12-21 17:13:47 +0100

    implement ThreadData header

--- core/datastore/ThreadData.h
+++ core/datastore/ThreadData.h
@@ -2,8 +2,21 @@
 #define CAE_PERFORMANCE_DATA_MONITORING_TOOL_THREADDATA_H
 
 
-class ThreadData {
+#include <string>
 
+using std::string;
+
+struct ThreadData {
+    string m_name;
+    bool m_isValid;
+    double m_sumRt;
+    double m_maxRt;
+    double m_avgRt;
+    double m_sumVs;
+    double m_sumIs;
+
+    ThreadData(string name, bool isValid, double sumRt, double maxRt, double avgRt, double sumVs,
+               double sumIs);
 };
 
 
