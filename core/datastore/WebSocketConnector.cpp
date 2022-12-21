0c10cf9d69fccf226a6dbffe61ffd24bff1b385f
Julius Gummersbach <gummersbach@se-rwth.de>
2022-12-21 17:23:19 +0100

    add header for dummy WebSocketConnector

--- core/datastore/WebSocketConnector.cpp
+++ core/datastore/WebSocketConnector.cpp
@@ -2,29 +2,13 @@
 #include <algorithm>
 #include <string>
 #include "DataManager.cpp"
+#include "WebSocketConnector.h"
 
 using json = nlohmann::json;
 
-/**
- * This Class is responsible for handling data received from the WebSocket.
- * It parses received data and hands it over to the DataManager
- */
-class WebSocketConnector {
-private:
-    DataManager dataManager;
+WebSocketConnector::WebSocketConnector(const DataManager &dataManager) : m_dataManager{std::move(dataManager)}{
+}
 
-public:
-    explicit WebSocketConnector(const DataManager &dataManager) : dataManager(std::move(dataManager)){
-
-    }
-
-    /**
-     * Is triggered when new data is received from the WebSocket
-     * Parses the received data to a dictionary and hands it over to the DataManager
-     *
-     * @param jsonString the data received as a String
-     */
-    void receiveData(std::string jsonString) {
-        dataManager.addThreadInfo(json::parse(jsonString))
-    }
-};
+void WebSocketConnector::receiveData(std::string jsonString) {
+    m_dataManager.addThreadInfo(json::parse(jsonString))
+}
