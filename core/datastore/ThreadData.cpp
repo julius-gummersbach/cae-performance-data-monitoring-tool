d44059a20a797bb96c89731e496bc9c8ded2cb5f
Julius Gummersbach <gummersbach@se-rwth.de>
2022-12-21 17:13:26 +0100

    implement ThreadData constructor

--- core/datastore/ThreadData.cpp
+++ core/datastore/ThreadData.cpp
@@ -1,3 +1,11 @@
 #include "ThreadData.h"
 
+#include <utility>
 
+using std::string;
+
+
+ThreadData::ThreadData(string name, bool isValid, double sumRt, double maxRt, double avgRt, double sumVs, double sumIs) :
+                       m_name(std::move(name)), m_isValid(isValid), m_sumRt(sumRt), m_maxRt(maxRt), m_avgRt(avgRt),
+                       m_sumVs(sumVs), m_sumIs(sumIs) {
+}
