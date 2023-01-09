14ae7913ec7c5cdcee24923eb1c526ae3a286aa6
jonas.boecker <boecker@se-rwth.de>
2023-01-09 15:35:41 +0100

    merge branch add-data-validation

--- core/datastore/WebSocketConnector.cpp
+++ core/datastore/WebSocketConnector.cpp
@@ -1,13 +1,10 @@
 #include "WebSocketConnector.h"
 
-WebSocketConnector::WebSocketConnector(const DataManager &dataManager){
 using json = nlohmann::json;
 
 WebSocketConnector::WebSocketConnector(const DataManager &dataManager) :
-m_dataManager{std::move(dataManager)}{
-}
+        m_dataManager{std::move(dataManager)}{}
 
-void WebSocketConnector::receiveData(const std::string &jsonString){
-void WebSocketConnector::receiveData(std::string jsonString) {
-    m_dataManager.addThreadInfo(json::parse(jsonString));
+void WebSocketConnector::receiveData(const std::string &jsonString) {
+  m_dataManager.addThreadInfo(json::parse(jsonString));
 }
