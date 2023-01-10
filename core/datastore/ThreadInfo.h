bc0c4350a6041f9daf41ae7f5b7aae7541c70cee
Julius Gummersbach <gummersbach@se-rwth.de>
2023-01-10 12:32:58 +0100

    add documentation to ThreadInfo

--- core/datastore/ThreadInfo.h
+++ core/datastore/ThreadInfo.h
@@ -1,7 +1,6 @@
 #ifndef CAE_PERFORMANCE_DATA_MONITORING_TOOL_THREADINFO_H
 #define CAE_PERFORMANCE_DATA_MONITORING_TOOL_THREADINFO_H
 
-
 #include <string>
 #include <list>
 #include "ThreadData.h"
@@ -13,16 +12,33 @@ using std::string;
  */
 class ThreadInfo {
 private:
+    /* unique identifier id of the Thread */
     long m_threadId;
+    /* name of the thread */
     string m_name;
+    /* frequency of the thread */
     double m_freq;
+    /* iterations the thread has gone through */
     int m_iterations;
+    /* overruns the thread has gone through */
     int m_overruns;
+    /* total runtime of the thread */
     double m_sumRt;
+    /* ThreadData objects associated with this thread */
     std::list<ThreadData> m_data;
 public:
     ThreadInfo(long threadId, string name, double freq);
 
+    /**
+     * adds a new ThreadData object to the m_data list
+     *
+     * @param name attribute of the to be created ThreadData object
+     * @param sumRt attribute of the to be created ThreadData object
+     * @param maxRt attribute of the to be created ThreadData object
+     * @param avgRt attribute of the to be created ThreadData object
+     * @param sumVs attribute of the to be created ThreadData object
+     * @param sumIs attribute of the to be created ThreadData object
+     */
     void addData(const string& name,
                  double sumRt,
                  double maxRt,
