16a8c9194dcc4cfdc22a0e05dfff59e94ceca56e
Simeon Burk <simeon.burk@alumni.fh-aachen.de>
2023-01-11 15:00:23 +0100

    First proof-of-concept of sending data from server.py to test.cpp with websocket

--- resources/schema.py
+++ resources/schema.py
@@ -1,22 +1,23 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
 class ThreadData:
-  name: str
-  sum_rt: float # [s]
-  max_rt: float # [s]
-  avg_rt: float # [ms]
-  sum_vs: float # [s]
-  sum_is: float # [s]
-    
-    
+    name: str
+    sum_rt: float  # [s]
+    max_rt: float  # [s]
+    avg_rt: float  # [ms]
+    sum_vs: float  # [s]
+    sum_is: float  # [s]
+
+
 @dataclass
 class ThreadInfo:
-  tid: str
-  name: str
-  freq: float
-  iterations: int
-  overruns: int
-  sum_rt: float     # [s]
-  data: List[ThreadData]
+    tid: str
+    name: str
+    freq: float
+    iterations: int
+    overruns: int
+    sum_rt: float  # [s]
+    data: List[ThreadData]
