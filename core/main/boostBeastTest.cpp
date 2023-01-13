d8fddcd378ad5111347d6e38e4932e202eaceaf3
Simeon Burk <simeon.burk@alumni.fh-aachen.de>
2023-01-13 10:12:36 +0100

    Other framework (boost) works now too, at least in concept. File may need to be build in Release Mode rather than Debug. Under Settings/Build, Execution, Deployment/CMake click on the plus to add a  release option to your run configurations

--- core/main/boostBeastTest.cpp
+++ core/main/boostBeastTest.cpp
@@ -6,7 +6,7 @@
 #include <boost/beast/websocket.hpp>
 #include <boost/asio/strand.hpp>
 #include <cstdlib>
-#include <functional>
+//#include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -20,15 +20,12 @@ using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 //------------------------------------------------------------------------------
 
 // Report a failure
-void
-fail(beast::error_code ec, char const* what)
-{
+void fail(beast::error_code ec, char const* what){
     std::cerr << what << ": " << ec.message() << "\n";
 }
 
 // Sends a WebSocket message and prints the response
-class session : public std::enable_shared_from_this<session>
-{
+class session : public std::enable_shared_from_this<session>{
     tcp::resolver resolver_;
     websocket::stream<beast::tcp_stream> ws_;
     beast::flat_buffer buffer_;
@@ -38,10 +35,7 @@ class session : public std::enable_shared_from_this<session>
 public:
     // Resolver and socket require an io_context
     explicit
-    session(net::io_context& ioc)
-            : resolver_(net::make_strand(ioc))
-            , ws_(net::make_strand(ioc))
-    {
+    session(net::io_context& ioc) : resolver_(net::make_strand(ioc)), ws_(net::make_strand(ioc)){
     }
 
     // Start the asynchronous operation
@@ -61,10 +55,8 @@ public:
     void on_resolve(beast::error_code ec, tcp::resolver::results_type results){
         if(ec)
             return fail(ec, "resolve");
-
         // Set the timeout for the operation
         beast::get_lowest_layer(ws_).expires_after(std::chrono::seconds(30));
-
         // Make the connection on the IP address we get from a lookup
         beast::get_lowest_layer(ws_).async_connect(
                 results,
@@ -76,23 +68,17 @@ public:
     void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep){
         if(ec)
             return fail(ec, "connect");
-
         // Turn off the timeout on the tcp_stream, because
         // the websocket stream has its own timeout system.
         beast::get_lowest_layer(ws_).expires_never();
-
         // Set suggested timeout settings for the websocket
-        ws_.set_option(
-                websocket::stream_base::timeout::suggested(
-                        beast::role_type::client));
+        ws_.set_option(websocket::stream_base::timeout::suggested(beast::role_type::client));
 
         // Set a decorator to change the User-Agent of the handshake
-        ws_.set_option(websocket::stream_base::decorator(
-                [](websocket::request_type& req)
-                {
-                    req.set(http::field::user_agent,
-                            std::string(BOOST_BEAST_VERSION_STRING) +
-                            " websocket-client-async");
+        ws_.set_option(websocket::stream_base::decorator([](websocket::request_type& req){
+            req.set(http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                    " websocket-client-async");
                 }));
 
         // Update the host_ string. This will provide the value of the
