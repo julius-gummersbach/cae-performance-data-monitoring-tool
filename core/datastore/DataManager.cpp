bd85c6a395be332690d6c31600709f2ec56d7173
jonas.boecker <boecker@se-rwth.de>
2023-01-19 13:58:46 +0100

    add correct namespace

--- core/datastore/DataManager.cpp
+++ core/datastore/DataManager.cpp
@@ -1,71 +1,74 @@
 #include "DataManager.hpp"
 
-void DataManager::addThreadInfo(const json &object) {
-  if (m_threadInfos.find(object["tid"]) == m_threadInfos.end()) {
-    ThreadInfo info = getInfo(object);
-    vector<json> data = object["data"];
-    for (const json &j: data) {
-      addThreadData(info, j);
-    }
-    m_threadInfos.insert_or_assign(info.getThreadId(), info);
-  } else {
-    if (std::basic_string.equal(m_threadInfos.at(object["tid"]).getName(),object["name"]) {
+namespace CAEMonitoringTool::DataStore {
+
+  void DataManager::addThreadInfo(const json &object) {
+    if (m_threadInfos.find(object["tid"]) == m_threadInfos.end()) {
+      ThreadInfo info = getInfo(object);
       vector<json> data = object["data"];
-      ThreadInfo &pInfo = m_threadInfos.at(object["tid"]);
       for (const json &j: data) {
-        addThreadData(pInfo, j);
+        addThreadData(info, j);
       }
+      m_threadInfos.insert_or_assign(info.getThreadId(), info);
     } else {
-      // TODO: warning here that id was not unique
+      if (std::basic_string.equal(m_threadInfos.at(object["tid"]).getName(), object["name"]) {
+        vector<json> data = object["data"];
+        ThreadInfo &pInfo = m_threadInfos.at(object["tid"]);
+        for (const json &j: data) {
+          addThreadData(pInfo, j);
+        }
+      } else {
+        // TODO: warning here that id was not unique
+      }
     }
   }
-}
 
-const pair<bool, ThreadInfo> &DataManager::getDataFromId(const string &id) {
+  const pair<bool, ThreadInfo> &DataManager::getDataFromId(const string &id) {
 
-  pair<bool, ThreadInfo> p{true,
-                           ThreadInfo{"foo",
-                                      "bar",
-                                      static_cast<double>(rand() % 100)}};
-  return p;
-
-  //TODO: comment back in once real data is usable and delete rng
-  /*
-  if (m_threadInfos.find(id) == m_threadInfos.end()) {
-    pair<bool, ThreadInfo> p{true, m_threadInfos.at(id)};
+    pair<bool, ThreadInfo> p{true,
+                             ThreadInfo{"foo",
+                                        "bar",
+                                        static_cast<double>(rand() % 100)}};
     return p;
-  } else {
-    pair<bool, ThreadInfo> p{false, ThreadInfo{0,nullptr,0}};
-    return p;
+
+    //TODO: comment back in once real data is usable and delete rng
+    /*
+    if (m_threadInfos.find(id) == m_threadInfos.end()) {
+      pair<bool, ThreadInfo> p{true, m_threadInfos.at(id)};
+      return p;
+    } else {
+      pair<bool, ThreadInfo> p{false, ThreadInfo{0,nullptr,0}};
+      return p;
+    }
+    */
   }
-  */
-}
 
-const pair<bool, ThreadInfo> &DataManager::operator[](const string &id) const {
-  return getDataFromId(id);
-}
+  const pair<bool, ThreadInfo> &DataManager::operator[](const string &id) const {
+    return getDataFromId(id);
+  }
 
-void DataManager::addThreadData(ThreadInfo &info, const json &j) {
-  string name = j["name"];
-  vector<double> sum_rt = j["sum_rt"];
-  vector<double> max_rt = j["max_rt"];
-  vector<double> avg_rt = j["avg_rt"];
-  double sum_vs = j["sum_vs"];
-  double sum_is = j["sum_is"];
-  info.addData(name, sum_rt.front(), max_rt.front(), avg_rt.front(), sum_vs, sum_is);
-}
+  void DataManager::addThreadData(ThreadInfo &info, const json &j) {
+    string name = j["name"];
+    vector<double> sum_rt = j["sum_rt"];
+    vector<double> max_rt = j["max_rt"];
+    vector<double> avg_rt = j["avg_rt"];
+    double sum_vs = j["sum_vs"];
+    double sum_is = j["sum_is"];
+    info.addData(name, sum_rt.front(), max_rt.front(), avg_rt.front(), sum_vs, sum_is);
+  }
 
-ThreadInfo DataManager::getInfo(const json &object) {
-  Graph::timePoint startTime = std::chrono::system_clock::now();
-  string tid = object["tid"];
-  string name = object["name"];
-  double freq = object["freq"];
+  ThreadInfo DataManager::getInfo(const json &object) {
+    Graph::timePoint startTime = std::chrono::system_clock::now();
+    string tid = object["tid"];
+    string name = object["name"];
+    double freq = object["freq"];
 
-  // TODO: determine what to do with these parameters
-  // int iterations = object["iterations");
-  // int overruns = object["overruns");
-  // vector<double> sum_rt = object["sum_rt");
+    // TODO: determine what to do with these parameters
+    // int iterations = object["iterations");
+    // int overruns = object["overruns");
+    // vector<double> sum_rt = object["sum_rt");
 
-  ThreadInfo info{tid, name, freq, startTime};
-  return info;
+    ThreadInfo info{tid, name, freq, startTime};
+    return info;
+  }
 }
--- core/datastore/DataManager.hpp
+++ core/datastore/DataManager.hpp
@@ -17,20 +17,22 @@ using std::cout;
 using std::endl;
 using std::pair;
 
+namespace CAEMonitoringTool::DataStore {
+
 /**
  * class to store all the ThreadInfos in a map sorted by their id given to it via a json object containing
  * the information from the websocket connector
  */
-class DataManager {
+  class DataManager {
 
-public:
+  public:
 
     /**
      * method to get new incoming date from a json library and store it in the map for later use
      *
      * @param threadInfo the json library coming from the websocket connector with the ThreadInfo objects
      */
-    void addThreadInfo(const json& threadInfo);
+    void addThreadInfo(const json &threadInfo);
 
     /**
      * overloading of the []-operator as a way to retrieve the ThreadInfo objects from the map via a given
@@ -41,9 +43,9 @@ public:
      * successful and a ThreadInfo object with the given id was found and returned or unsuccessful and a
      * dummy ThreadInfo object is created and returned with null-values as attributes
      */
-    const pair<bool,ThreadInfo>& operator[](const string& id) const;
+    const pair<bool, ThreadInfo> &operator[](const string &id) const;
 
-private:
+  private:
 
     /**
      * map to assign a ThreadInfo object to a long value to store the data from the websocket connector
@@ -56,21 +58,22 @@ private:
      * @return boolean-ThreadInfo pair in which the first parameter indicates if the process was successful
      * and the second is the data object from the map or a dummy object
      */
-    [[nodiscard]] static const pair<bool, ThreadInfo>& getDataFromId(const string& id);
+    [[nodiscard]] static const pair<bool, ThreadInfo> &getDataFromId(const string &id);
 
     /**
      * method to get the ThreadData parameters from a json and add it to the given ThreadInfo
      * @param info ThreadInfo object to add the additional data to
      * @param j json representation of the data of the ThreadData object
      */
-    static void addThreadData(ThreadInfo &info, const json &j) ;
+    static void addThreadData(ThreadInfo &info, const json &j);
 
     /**
      * method to retrieve data from the json representation of a ThreadInfo object
      * @param object json representation of the ThreadInfo
      * @return ThreadInfo object with the given data from the json library
      */
-    [[nodiscard]] static ThreadInfo getInfo(const json &object) ;
-};
+    [[nodiscard]] static ThreadInfo getInfo(const json &object);
+  };
 
+}
 #endif //CAE_PERFORMANCE_DATA_MONITORING_TOOL_DATAMANAGER_HPP
