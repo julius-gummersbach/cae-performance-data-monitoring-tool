68a612ef11fe0d94953bc10f70269da93bb31915
jonas.boecker <boecker@se-rwth.de>
2023-01-19 16:59:54 +0100

    fix merge conflicts

--- core/datastore/DataManager.cpp
+++ core/datastore/DataManager.cpp
@@ -2,11 +2,11 @@
 
 namespace CAEMonitoringTool::DataStore {
 
-  std::deque<double> DataManager::getData(const ThreadInfo &info,
-                                                                     const std::vector<std::shared_ptr<ThreadData>> &data,
-                                                                     const Graph::time_point &startTime,
-                                                                     const Graph::time_delta &length,
-                                                                     const DataManager::getterFunc &func) {
+  static std::deque<double> DataManager::getData(const ThreadInfo &info,
+                                                 const std::vector<std::shared_ptr<ThreadData>> &data,
+                                                 const DataProcessing::time_point &startTime,
+                                                 const DataProcessing::time_delta &length,
+                                                 const DataManager::getterFunc &func) {
     long long startIndex = (long long) ((startTime - info.getStartTime()) * info.getFreq() / 1000000000).count();
     if (data.size() < startIndex) {
       return std::deque<double>{};
@@ -21,50 +21,54 @@ namespace CAEMonitoringTool::DataStore {
   }
 
   std::deque<double> DataManager::getPointsInIntervall(const std::string &id,
-                                                                                  const Graph::time_point &startTime,
-                                                                                  const Graph::time_delta &length,
-                                                                                  const Graph::GraphDataType &type) const {
+                                                       const DataProcessing::time_point &startTime,
+                                                       const DataProcessing::time_delta &length,
+                                                       const DataProcessing::GraphDataType &type) const {
 
     const ThreadInfo info{m_threadInfos.at(id)};
     const std::vector<std::shared_ptr<ThreadData>> data{info.getData()};
 
     switch (type) {
-      case Graph::GraphDataType::DATA_SUM_RT : {
+      case DataProcessing::GraphDataType::DATA_SUM_RT : {
         std::deque<double> result{
                 getData(info, data, startTime, length, DataManager::getSumRT)};
         return result;
       }
-      case Graph::GraphDataType::MAX_RT : {
+      case DataProcessing::GraphDataType::MAX_RT : {
         std::deque<double> result{
                 getData(info, data, startTime, length, DataManager::getMaxRT)};
         return result;
       }
-      case Graph::GraphDataType::AVG_RT : {
+      case DataProcessing::GraphDataType::AVG_RT : {
         std::deque<double> result{
                 getData(info, data, startTime, length, DataManager::getAvgRT)};
         return result;
       }
-      case Graph::GraphDataType::SUM_VS : {
+      case DataProcessing::GraphDataType::SUM_VS : {
         std::deque<double> result{
                 getData(info, data, startTime, length, DataManager::getSumVS)};
         return result;
       }
-      case Graph::GraphDataType::SUM_IS : {
+      case DataProcessing::GraphDataType::SUM_IS : {
         std::deque<double> result{
                 getData(info, data, startTime, length, DataManager::getSumIS)};
         return result;
       }
-      case Graph::GraphDataType::INFO_SUM_RT : {
+      case DataProcessing::GraphDataType::INFO_SUM_RT : {
         return std::deque<double>{info.getSumRt()};
       }
-      case Graph::GraphDataType::FREQ : {
+      case DataProcessing::GraphDataType::FREQ : {
         return std::deque<double>{info.getFreq()};
       }
-      case Graph::GraphDataType::ITERATIONS : {
+      case DataProcessing::GraphDataType::ITERATIONS : {
         return std::deque<double>{(double) info.getIterations()};
       }
-      case Graph::GraphDataType::OVERRUNS : {
+      case DataProcessing::GraphDataType::OVERRUNS : {
         return std::deque<double>{(double) info.getOverruns()};
+      }
+    }
+  }
+
   void DataManager::addThreadInfo(const json &object) {
     if (m_threadInfos.find(object["tid"]) == m_threadInfos.end()) {
       ThreadInfo info = getInfo(object);
@@ -109,4 +113,7 @@ namespace CAEMonitoringTool::DataStore {
     ThreadInfo info{tid, name, freq, startTime, freq, iterations, overruns};
     return info;
   }
+}
+}
+}
 }
--- core/datastore/DataManager.hpp
+++ core/datastore/DataManager.hpp
@@ -14,83 +14,64 @@ namespace CAEMonitoringTool::DataStore {
 
   using json = nlohmann::json;
 
-
-using json = nlohmann::json;
-using std::string;
-using std::unordered_map;
-using std::vector;
-using std::cout;
-using std::endl;
-using std::pair;
-
-namespace CAEMonitoringTool::DataStore {
-
-/**
- * class to store all the ThreadInfos in a map sorted by their id given to it via a json object containing
- * the information from the websocket connector
- */
-  class DataManager {
-
-  public:
+  using json = nlohmann::json;
+  using std::string;
+  using std::unordered_map;
+  using std::vector;
+  using std::cout;
+  using std::endl;
+  using std::pair;
 
     /**
-     * method to get new incoming date from a json library and store it in the map for later use
-     *
-     * @param threadInfo the json library coming from the websocket connector with the ThreadInfo objects
+     * class to store all the ThreadInfos in a map sorted by their id given to it via a json object containing
+     * the information from the websocket connector
      */
-    void addThreadInfo(const json &threadInfo);
+    class DataManager {
 
-  private:
+    public:
 
-    /**
-     * map to assign a ThreadInfo object to a long value to store the data from the websocket connector
-     */
-    unordered_map<string, ThreadInfo> m_threadInfos;
-    /**
-     * overloading of the []-operator as a way to retrieve the ThreadInfo objects from the map via a given
-     * id
-     *
-     * @param id id of the ThreadInfo object to retrieve
-     * @return a boolean-ThreadInfo pair in which the first boolean value indicates if the request was
-     * successful and a ThreadInfo object with the given id was found and returned or unsuccessful and a
-     * dummy ThreadInfo object is created and returned with null-values as attributes
-     */
-    const std::pair<bool, ThreadInfo> &operator[](const string &id) const;
+      /**
+       * method to get new incoming date from a json library and store it in the map for later use
+       *
+       * @param threadInfo the json library coming from the websocket connector with the ThreadInfo objects
+       */
+      void addThreadInfo(const json &threadInfo);
 
-    /**
-     * method for the graph-expressions to get the data points for the graphs from the data manager in a given time
-     * interval
-     *
-     * @param id id of the threadinfo object the data has to be retrieved from
-     * @param startTime start time point of the time interval
-     * @param length length of the time interval in nanoseconds
-     * @param type data type of the graph
-     * @return a list of points with the values of the given type in the given time interval
-     */
-    std::deque<double> getPointsInIntervall(const string &id,
-                                            const Graph::time_point &startTime,
-                                            const Graph::time_delta &length,
-                                            const Graph::GraphDataType &type) const;
+    private:
 
-  private:
+      /**
+       * general function pointer for the getter functions for the attributes of the threaddata objects
+       */
+      typedef const double &(*getterFunc)(const ThreadData &data);
 
-    /**
-     * general function pointer for the getter functions for the attributes of the threaddata objects
-     */
-    typedef const double&(*getterFunc)(const ThreadData &data);
+      /**
+       * map to assign a ThreadInfo object to a long value to store the data from the websocket connector
+       */
+      unordered_map<string, ThreadInfo> m_threadInfos;
 
-    /**
-     * map to assign a ThreadInfo object to a long value to store the data from the websocket connector
-     */
-    std::unordered_map<string, ThreadInfo> m_threadInfos;
+      /**
+       * method to retrieve data from the json representation of a ThreadInfo object
+       * @param object json representation of the ThreadInfo
+       * @return ThreadInfo object with the given data from the json library
+       */
+      [[nodiscard]] static ThreadInfo getInfo(const json &object);
 
-    /**
-     * method to be called by the overloaded []-operator to retrieve data from the map
-     * @param id id of the data to be retrieved
-     * @return boolean-ThreadInfo pair in which the first parameter indicates if the process was successful
-     * and the second is the data object from the map or a dummy object
-     */
-    [[nodiscard]] static const std::pair<bool, ThreadInfo> &getDataFromId(const string &id);
+      /**
+       * method to calculate all the threaddata objects in the given time intervall and return the requestes data type
+       * from those using the passed function
+       *
+       * @param info threadinfo object the data the data has to be retrieved from
+       * @param data list of threaddata objects the attributes have to be retrieves from
+       * @param startTime start time point of the time interval
+       * @param length length of the time interval in nanoseconds
+       * @param func pointer to the getter function to get the right attribute form the thread data objects
+       * @return a list of points with the values of the given type in the given time interval
+       */
+      static std::deque<double> getData(const ThreadInfo &info,
+                                        const std::vector<std::shared_ptr<ThreadData>> &data,
+                                        const DataProcessing::time_point &startTime,
+                                        const DataProcessing::time_delta &length,
+                                        const getterFunc &func);
 
     /**
      * method to get the ThreadData parameters from a json and add it to the given ThreadInfo
@@ -98,34 +79,6 @@ namespace CAEMonitoringTool::DataStore {
      * @param j json representation of the data of the ThreadData object
      */
     static void addThreadData(ThreadInfo &info, const json &j);
-     * @param object json representation of the data of the ThreadData object
-     */
-    static void addThreadData(ThreadInfo &info, const json &object);
-
-    /**
-     * method to retrieve data from the json representation of a ThreadInfo object
-     * @param object json representation of the ThreadInfo
-     * @return ThreadInfo object with the given data from the json library
-     */
-    [[nodiscard]] static ThreadInfo getInfo(const json &object);
-
-    /**
-     * method to calculate all the threaddata objects in the given time intervall and return the requestes data type
-     * from those using the passed function
-     *
-     * @param info threadinfo object the data the data has to be retrieved from
-     * @param data list of threaddata objects the attributes have to be retrieves from
-     * @param startTime start time point of the time interval
-     * @param length length of the time interval in nanoseconds
-     * @param func pointer to the getter function to get the right attribute form the thread data objects
-     * @return a list of points with the values of the given type in the given time interval
-     */
-    static std::deque<double> getData(const ThreadInfo &info,
-                               const std::vector<std::shared_ptr<ThreadData>> &data,
-                               const Graph::time_point &startTime,
-                               const Graph::time_delta &length,
-                               const getterFunc &func);
   };
-
 }
 #endif //CAE_PERFORMANCE_DATA_MONITORING_TOOL_DATAMANAGER_HPP
