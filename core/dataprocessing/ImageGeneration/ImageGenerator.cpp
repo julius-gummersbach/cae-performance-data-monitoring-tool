cb9da1ad883dd5fbc58cf62c2bb5659230e6a59f
Simeon Burk <simeon.burk@alumni.fh-aachen.de>
2023-02-01 14:35:05 +0100

    Removed comments, added axis label

--- core/dataprocessing/ImageGeneration/ImageGenerator.cpp
+++ core/dataprocessing/ImageGeneration/ImageGenerator.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include "ImageGenerator.hpp"
 #include "memory"
-#include "iostream"
 
 using namespace SVGChart;
 using std::string;
@@ -11,49 +10,7 @@ using std::pair;
 using std::vector;
 using std::make_unique;
 
-
-
 namespace CAEMonitoringTool::DataProcessing {
-
-    void test(PPlot &ioPPlot) {
-        ioPPlot.mPlotBackground.mTitle = "Mein graph XD";
-        PlotData *theX1 = new PlotData();
-        PlotData *theY1 = new PlotData();
-
-        PlotData *theX2 = new PlotData();
-        PlotData *theY2 = new PlotData();
-
-        auto *theX3 = new PlotData();
-        auto *theY3 = new PlotData();
-
-        float theFac = 100.0f / (100 * 100 * 100);
-        theX1->push_back(0);
-        theY1->push_back(0);
-        for (int theI = 0; theI <= 10; theI++) {
-            theX1->push_back(0.001 * theI);
-            theY1->push_back(0.01 + theFac * theI * theI * theI);
-
-            theX2->push_back(0.001 * theI);
-            theY2->push_back(0.01 + theFac * theI * theI * theI + 0.02);
-        }
-        LegendData *theLegend = new LegendData();
-        theLegend->mName = "Harold";
-        theLegend->mColor = PColor(100, 100, 200);
-
-        LegendData *theLegend2 = new LegendData();
-        theLegend2->mName = "Berta";
-        theLegend2->mColor = PColor(255, 0, 0);
-        //PlotDataSelection *thePlotDataSelection = new PlotDataSelection ();
-        //ioPPlot.mPlotDataContainer.AddXYPlot (theX1, theY1, theLegend, 0, thePlotDataSelection);
-        ioPPlot.mPlotDataContainer.AddXYPlot(theX1, theY1, theLegend);
-        ioPPlot.mPlotDataContainer.AddXYPlot(theX2, theY2, theLegend2);
-
-        ioPPlot.mMargins.mLeft = 90;
-        ioPPlot.mMargins.mTop = 50;
-        ioPPlot.mMargins.mBottom = 50;
-        ioPPlot.mMargins.mRight = 50;
-    }
-
 
     void plotIt(PPlot &ioPPlot, const vector<pair<int,double>>& points){
         ioPPlot.mPlotBackground.mTitle = "Unser wunderschöner Graph";
@@ -61,7 +18,6 @@ namespace CAEMonitoringTool::DataProcessing {
         auto *yValues = new PlotData();
         xValues->push_back(0);
         yValues->push_back(0);
-
         for(auto pair : points){
             xValues->push_back(pair.first * 16);
             yValues->push_back(pair.second);
@@ -70,24 +26,24 @@ namespace CAEMonitoringTool::DataProcessing {
         legend->mName = "Unsere Beispiel-Daten";
         legend->mColor = PColor(100, 100, 200);
 
-        //PlotDataSelection *thePlotDataSelection = new PlotDataSelection ();
-        //ioPPlot.mPlotDataContainer.AddXYPlot (theX1, theY1, theLegend, 0, thePlotDataSelection);
         ioPPlot.mPlotDataContainer.AddXYPlot(xValues, yValues, legend);
 
         ioPPlot.mMargins.mLeft = 90;
         ioPPlot.mMargins.mTop = 50;
         ioPPlot.mMargins.mBottom = 50;
         ioPPlot.mMargins.mRight = 50;
+
+        ioPPlot.mXAxisSetup.mLabel = "Zeit in Millisekunden";
+        ioPPlot.mYAxisSetup.mLabel = "Laufzeit in Millisekunden";
+        ioPPlot.mGridInfo.mYGridOn = true;
     }
 
     std::string ImageGenerator::generateImage(const vector<std::pair<int, double>> &points) {
         string path{"test5.svg"};
         PPlot pplot;
-        //test(pplot);
         plotIt(pplot,points);
         long width = points.size() * 16;
-        std::cout << points.size() << std::endl;
-        SVGPainter painter(width, 450); //490, 450
+        SVGPainter painter(width, 450);
         pplot.Draw(painter);
         painter.writeFile(path);
         return path;
