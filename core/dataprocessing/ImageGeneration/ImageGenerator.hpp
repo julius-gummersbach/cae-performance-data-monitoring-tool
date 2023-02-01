f30a41cd4b47ce4f72b284c46076853f39f60ad5
Simeon Burk <simeon.burk@alumni.fh-aachen.de>
2023-02-01 13:55:56 +0100

    Implemented first version of Image Generator

---
+++ core/dataprocessing/ImageGeneration/ImageGenerator.hpp
@@ -0,0 +1,20 @@
+//
+// Created by CAE-DUALSTUDIS on 01.02.2023.
+//
+
+#ifndef CAE_PERFORMANCE_DATA_MONITORING_TOOL_IMAGEGENERATOR_HPP
+#define CAE_PERFORMANCE_DATA_MONITORING_TOOL_IMAGEGENERATOR_HPP
+
+#include "string"
+#include "vector"
+
+
+namespace CAEMonitoringTool::DataProcessing {
+    class ImageGenerator {
+    public:
+        static std::string generateImage(const std::vector <std::pair<int, double>> &points);
+    };
+}
+
+
+#endif //CAE_PERFORMANCE_DATA_MONITORING_TOOL_IMAGEGENERATOR_HPP
